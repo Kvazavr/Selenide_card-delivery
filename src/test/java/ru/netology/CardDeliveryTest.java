94e7711a9b6615ca45c8394780ae1f234555d0c8
Elena Nekrasova <kvazavr2108@gmail.com>
2023-05-05 22:51:13 +0400

    Modified test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,25 +4,40 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAmount;
 import java.util.Date;
 
 public class CardDeliveryTest {
     @Test
-    void shouldBeSuccessCardOrder() {
+    void shouldBeSuccessCardOrder() throws InterruptedException {
         open("http://localhost:9999/");
-        $("[data-test-id=city]").click();
+//        $("[data-test-id=city]").click();
         $("[type = text]").setValue("Ижевск");
         $("[data-test-id=date]").click();
-        $("[value]").clear();
-        $("[data-test-id=name]").click();
+        $("[data-test-id=date] [value]").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] [value]").sendKeys(Keys.BACK_SPACE);
+        LocalDateTime currentDay = LocalDateTime.now();
+        LocalDateTime validDate = currentDay.plus(3, DAYS);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String format = validDate.format(dateTimeFormatter);
+        $("[data-test-id=date] [value]").setValue(format);
+//        Thread.sleep(10000);
+//        $("[data-test-id=name]").click();
         $("[name = name]").setValue("Некрасова Елена");
-        $("[data-test-id=phone]").click();
+//        $("[data-test-id=phone]").click();
         $("[name = phone]").setValue("+79085552211");
         $("[data-test-id=agreement]").click();
         $("[type = button]").click();
+
 
 //        Date date = new Date();
 //        System.out.println(date);
