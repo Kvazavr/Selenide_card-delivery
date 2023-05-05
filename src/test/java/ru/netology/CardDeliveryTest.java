856dc8f10e09e56df37594a72701058367f47652
Elena Nekrasova <kvazavr@yandex.ru>
2023-05-05 15:15:50 +0400

    Added test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -4,19 +4,32 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.CollectionCondition.*;
+
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 public class CardDeliveryTest {
     @Test
     void shouldBeSuccessCardOrder() {
-                open("http://localhost:9999/");
+        open("http://localhost:9999/");
+        $("[data-test-id=city]").click();
+        $("[type = text]").setValue("Ижевск");
+        $("[data-test-id=date]").click();
+        $("[value]").clear();
+        $("[data-test-id=name]").click();
+        $("[name = name]").setValue("Некрасова Елена");
+        $("[data-test-id=phone]").click();
+        $("[name = phone]").setValue("+79085552211");
+        $("[data-test-id=agreement]").click();
+        $("[type = button]").click();
 
+//        Date date = new Date();
+//        System.out.println(date);
+//        $("[data-test-id=date]").clear();
+//        $("input value").setValue("10.05.2023");
 
     }
-
-
-
-
 
 
 }
