cdd0c21ea7fbc7166aa62f31239ba9d1e761a723
Elena Nekrasova <kvazavr@yandex.ru>
2023-05-05 12:12:50 +0400

    Initial commit

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+import org.junit.jupiter.api.Test;
+
+public class CardDeliveryTest {
+    @Test
+    void shouldBeSuccessCardOrder() {
+                open("http://localhost:9999/");
+
+
+    }
+
+
+
+
+
+
+}
